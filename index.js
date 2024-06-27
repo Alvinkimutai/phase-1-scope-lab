29c88aefabeef7eda42b2278fba914e8e7465fcf
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-06-27 12:53:39 +0300

    learns the scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+//function to uppercase
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+//function to setBestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+//function to overwriteBestCustomer
+function overwriteBestCustomer(){
+    bestCustomer ="maybe bob";
+    
+}//function to changeLeastFavoriteCustomer
+const leastFavoriteCustomer = 'Eston';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Alvin"
+}
+
